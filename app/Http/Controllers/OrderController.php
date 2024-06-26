6dab51c5b936c186b4aa20ae13db98bbda30849b
sigmeh <guenael.inard@le-campus-numerique.fr>
2024-06-26 11:33:36 +0200

    Adjusted store method in OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\UpdateOrderRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class OrderController extends Controller
 {
